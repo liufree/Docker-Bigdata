70141e4d4b5df120a61202d34f89a39264097a86
liufree <liufreeo@gmail.com>
2023-02-03 11:46:52 +0800

    [feat]:add prometheus

--- mysql/mysql-init/grant_all.sql
+++ mysql/mysql-init/grant_all.sql
@@ -16,3 +16,17 @@ grant all on bigdata.* to "liufree"@"%" with grant option;
 */
 /* 高版本授权bigdata数据库权限 caching_sha2_password */
 grant all on *.* to "liufree"@"%" with grant option;
+
+/*
+prometheus-exporter专用账号
+*/
+/* 高版本mysql授权远程访问 caching_sha2_password*/
+create user 'prom'@'%' identified by  'prometheus';
+grant all privileges on *.* to 'prom'@'%'  with grant option;
+flush privileges;
+
+/* 高版本授权bigdata数据库权限 
+grant all on bigdata.* to "liufree"@"%" with grant option;
+*/
+/* 高版本授权bigdata数据库权限 caching_sha2_password */
+grant all on *.* to "prom"@"%" with grant option;
